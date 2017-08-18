6802919e633200efffeac97b393c88dc68011786
christianarredondo <christianarredondo@utexas.edu>
2017-08-18 11:12:37 -0500

    finish exercises Type Assertions and Arrow Functions

--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,23 +1,49 @@
 
 
-// EXERCISE 1 - HELLO WORLD (to compile using tsc CLI)
-function log(message) {
-  console.log(message);
+// // EXERCISE 1 - HELLO WORLD (to compile using tsc CLI)
+// function log(message) {
+//   console.log(message);
+// }
+
+// var message = 'Hello World';
+// log(message);
+
+// // EXERCISE 2 - DECLARING VARIABLES
+// var number = 1;
+// let count = 2;
+
+// function doSomething () {
+//   for (var i = 0; i < 5; i++) {
+//     console.log(i)
+//   }
+
+//   console.log('Finally: ' + i);
+// }
+
+// doSomething();
+
+// // EXERCISE 3 - TYPES
+//     // let counter = 5;
+//     // counter = 'a';
+
+// let a: number;
+
+// const ColorRed = 0;
+// const ColorGreen = 1;
+// const ColorBlue = 2;
+
+// enum Color { Red = 0, Green = 1, Blue = 2, Purple = 3 };
+// let backgroundColor = Color.Green;
+// console.log(Color);
+
+// // EXERCISE 4 - TYPE ASSERTIONS
+// let message;
+// let endsWithC = (<string>message).endsWith('c')
+// let alternativeWay = (message as string).endsWith('c')
+
+//  EXERCISE 5 - ARROW FUNCTIONS
+let log = function (message) {
+  console.log(message)
 }
 
-var message = 'Hello World';
-log(message);
-
-// EXERCISE 2 - DECLARING VARIABLES
-var number = 1;
-let count = 2;
-
-function doSomething () {
-  for (var i = 0; i < 5; i++) {
-    console.log(i)
-  }
-
-  console.log('Finally: ' + i);
-}
-
-doSomething();
+let doLog = (message) => console.log(message);
